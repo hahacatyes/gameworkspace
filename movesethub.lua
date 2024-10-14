714371c0c3cb4c91a307c99d2a273b8c55ec497c
hahacatyes <cattosoftworks@onmail.com>
2024-10-13 22:45:33 -0400

    Update movesethub.lua

--- movesethub.lua
+++ movesethub.lua
@@ -2,7 +2,7 @@ local DrRayLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/
 
 local window = DrRayLibrary:Load("Moveset Hub", "Default")
 
-local tab = DrRayLibrary.newTab("Information", "rbxassetid://7731404883")
+local tab = DrRayLibrary.newTab("Information", "")
 
 tab.newButton("Owner: voidyyy0", "Copies the owner's username", function()
 setclipboard("voidyyy0")
@@ -12,7 +12,7 @@ tab.newButton("Discord Server", "Copies the discord server link.", function()
 setclipboard("https://discord.gg/Vmu9rUKn9h")
 end)
 
-local tab = DrRayLibrary.newTab("Movesets", "rbxassetid://15872631749")
+local tab = DrRayLibrary.newTab("Movesets", "15872631749")
 
 tab.newButton("Sukuna Moveset - Atomic BladeMaster", "Copies the owner's", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zyrask/Nexus-Base/main/atomic-blademaster%20to%20sukuna"))()
