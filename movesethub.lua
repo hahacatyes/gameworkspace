93136d7fca7771b41a067ee60d221cd502122ea6
hahacatyes <cattosoftworks@onmail.com>
2024-10-13 23:13:52 -0400

    Update movesethub.lua

--- movesethub.lua
+++ movesethub.lua
@@ -8,12 +8,40 @@ tab.newButton("Owner: voidyyy0", "Copies the owner's username", function()
 setclipboard("voidyyy0")
 end)
 
-tab.newButton("Discord Server", "Copies the discord server link.", function()
+tab.newButton("Discord Server", "Link will be copied, you can report bugs and stuff there.", function()
 setclipboard("https://discord.gg/Vmu9rUKn9h")
 end)
 
-local tab = DrRayLibrary.newTab("Movesets", "15872631749")
+local tab = DrRayLibrary.newTab("Movesets", "15011943566")
 
-tab.newButton("Sukuna Moveset - Atomic BladeMaster", "Copies the owner's", function()
+tab.newButton("Sukuna Moveset", "Use Blade Master.", function()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/zyrask/Nexus-Base/main/atomic-blademaster%20to%20sukuna"))()
+end)
+
+tab.newButton("Unsealed Gojo Moveset", "Use Saitama.", function()
+loadstring(game:HttpGet("https://pastebin.com/raw/zF6Rdky0"))()
+end)
+
+tab.newButton("Sans Moveset", "Use Wild Psychic.", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Qaiddanial2904/ROBLOX-FREAKY-GOJO-REAL/refs/heads/main/SANS%20%5BREDACTED%5D"))()
+end)
+
+tab.newButton("Child Emperor Moveset", "Use The Strongest Hero.", function()
+loadstring(game:HttpGet("https://pastebin.com/raw/CaXFZ2S2"))()
+end)
+
+tab.newButton("Jun Moveset", "Use The Strongest Hero.", function()
+loadstring(game:HttpGet('https://raw.githubusercontent.com/Kenjihin69/Kenjihin69/refs/heads/main/Tp%20exploit%20saitama%20to%20jun'))()
+end)
+
+tab.newButton("Dio Moveset", "Use Hero Hunter.", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Medley-Taboritsky/RobloxScripting/refs/heads/main/DIO_Garou_TSB"))()
+end)
+
+tab.newButton("Jester Moveset", "Use Hero Hunter.", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Reapvitalized/TSB/refs/heads/main/ARCAURA.lua"))()
+end)
+
+tab.newButton("Jester Moveset", "Use Hero Hunter.", function()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Reapvitalized/TSB/refs/heads/main/ARCAURA.lua"))()
 end)
