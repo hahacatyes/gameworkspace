1a7dba880a7685d29cd5ad94eef02d7ae3e184e2
hahacatyes <cattosoftworks@onmail.com>
2024-11-25 16:14:59 -0500

    Update fekj.lua

--- fekj.lua
+++ fekj.lua
@@ -1,48 +1,35 @@
-local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+local Rayfield = loadstring(game:HttpGet('https://sirius.menu/rayfield'))()
 
-local Window = OrionLib:MakeWindow({
-    Name = "FE KJ Premium Key System",
-    HidePremium = false,
-    SaveConfig = true,
-    ConfigFolder = "ZenonPremium"
+local Window = Rayfield:CreateWindow({
+   Name = "Premium FE KJ",
+   Icon = dna-off, -- Icon in Topbar. Can use Lucide Icons (string) or Roblox Image (number). 0 to use no icon (default).
+   LoadingTitle = "Premium FE KJ",
+   LoadingSubtitle = "By voidyyy0",
+   Theme = "Amethyst", -- Check https://docs.sirius.menu/rayfield/configuration/themes
+
+   DisableRayfieldPrompts = false,
+   DisableBuildWarnings = true, -- Prevents Rayfield from warning when the script has a version mismatch with the interface
+
+   ConfigurationSaving = {
+      Enabled = true,
+      FolderName = nil, -- Create a custom folder for your hub/game
+      FileName = "KJ2"
+   },
+
+   Discord = {
+      Enabled = true, -- Prompt the user to join your Discord server if their executor supports it
+      Invite = "bf3qJUwkYK", -- The Discord invite code, do not include discord.gg/. E.g. discord.gg/ABCD would be ABCD
+      RememberJoins = false -- Set this to false to make them join the discord every time they load it up
+   },
+
+   KeySystem = true, -- Set this to true to use our key system
+   KeySettings = {
+      Title = "Premium Access",
+      Subtitle = "Key System",
+      Note = "Premium only, get the key from premium-key channel.", -- Use this to tell the user how to get a key
+      FileName = "KeySave", -- It is recommended to use something unique as other scripts using Rayfield may overwrite your key file
+      SaveKey = true, -- The user's key will be saved, but if you change the key, they will be unable to use your script
+      GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+      Key = {"Z3N0N_PR3M321"} -- List of keys that will be accepted by the system, can be RAW file links (pastebin, github etc) or simple strings ("hello","key22")
+   }
 })
-
-local Tab = Window:MakeTab({
-    Name = "Premium Key",
-    Icon = "rbxassetid://4483345875",
-    PremiumOnly = false
-})
-
-Tab:AddParagraph("KJ FE Premium Access", "🎉 Enter the key you received from Discord to gain access! 🎉\n\nEnsure the key is typed correctly to avoid errors. If you haven’t received a key yet, click 'Get Key' to join our Discord server.")
-Tab:AddButton({
-    Name = "Get Key",
-    Callback = function()
-        setclipboard("https://discord.gg/bf3qJUwkYK")
-        OrionLib:MakeNotification({
-            Name = "Discord Invite",
-            Content = "Discord invite link copied to clipboard. Join to get your key!",
-            Image = "rbxassetid://4483345875",
-            Time = 5
-        })
-    end
-})
-
-Tab:AddTextbox({
-    Name = "Enter Key",
-    Default = "",
-    TextDisappear = true,
-    Callback = function(Value)
-        if Value == "Z3N0N_PR3M321" then
-            loadstring(game:HttpGet("https://raw.githubusercontent.com/Repcoders/voidy.lol/refs/heads/main/html1.lua",true))()
-        else
-            OrionLib:MakeNotification({
-                Name = "Invalid Key",
-                Content = "❌ The key you entered is invalid. Please check and try again. ❌",
-                Image = "rbxassetid://4483345875",
-                Time = 5
-            })
-        end
-    end
-})
-
-OrionLib:Init()
